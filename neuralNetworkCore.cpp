b758816fdd4bc7bc980dd9d70b6aa2ff5aa1667b
hughechr <hughechr@cvut.cz>
2025-01-03 16:07:58 +0100

    Make first commit

---
+++ neuralNetworkCore.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Christian on 1/3/2025.
+//
+
+#include "neuralNetworkCore.h"
---
+++ neuralNetworkCore.h
@@ -0,0 +1,16 @@
+//
+// Created by Christian on 1/3/2025.
+//
+
+#ifndef NEURALNETWORKCORE_H
+#define NEURALNETWORKCORE_H
+
+
+
+class neuralNetworkCore {
+
+};
+
+
+
+#endif //NEURALNETWORKCORE_H
